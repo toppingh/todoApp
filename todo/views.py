774cb39a31b3f8a6f4199b8e57a79e87ee0040a5
ekdms <reyna11@naver.com>
2023-07-04 00:29:39 +0900

    todo list files upload

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
+def list(request):
+    todos = Todo.objects.filter(complete=False)
+    return render(request, 'todo/list.html', {'todos':todos})
